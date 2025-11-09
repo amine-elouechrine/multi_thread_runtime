904628000cc84a22518601dd0fdd1b91f945c6ae
Amine <amine@kali.amine>
2025-11-09 11:00:31 -0500

    Initial commit

---
+++ lab6/src/tasks_implem.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include "tasks_implem.h"
+#include "debug.h"
+#include <stdlib.h>
+tasks_queue_t *tqueue = NULL;
+pthread_t *workers;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+
+void create_queues(void)
+{
+    tqueue = create_tasks_queue();
+}
+
+void delete_queues(void)
+{
+    free_tasks_queue(tqueue);
+}    
+
+void *worker_func(void *arg){//consumer
+    //PRINT_DEBUG(10, "Worker thread started.\n");
+    task_t *new_task;
+    while(1){
+        pthread_mutex_lock(&mutex);
+            while(tqueue->index==0){
+                pthread_cond_wait(&empty,&mutex);
+            }
+            //new task
+            new_task = get_task_to_execute();
+            pthread_cond_signal(&full);
+        pthread_mutex_unlock(&mutex);
+
+
+        if(new_task!=NULL){
+            int result = exec_task(new_task);
+            if(result == TASK_COMPLETED){
+                terminate_task(new_task);
+            }
+        }
+    }
+
+}
+
+void create_thread_pool(void)
+{   
+    workers = malloc(sizeof(pthread_t)*THREAD_COUNT);
+    if(workers == NULL){
+        perror("ALLOCATION ERROR");
+    }
+
+
+    for (size_t i = 0; i < THREAD_COUNT; i++)
+    {
+        /* code */
+       if(pthread_create(&workers[i],NULL,worker_func,NULL)!=0){
+            perror("THREAD CREATION PROBLEM\n");
+       }
+        
+    }
+
+}
+
+void dispatch_task(task_t *t)
+{   
+    pthread_mutex_lock(&mutex);
+        while(tqueue->index == tqueue->task_buf_size){
+            pthread_cond_wait(&full,&mutex);
+        }
+        //empty slot
+       enqueue_task(tqueue, t); 
+       pthread_cond_signal(&empty);
+    pthread_mutex_unlock(&mutex);
+    
+}
+
+task_t* get_task_to_execute(void)
+{
+    return dequeue_task(tqueue);
+}
+
+unsigned int exec_task(task_t *t)
+{
+    t->step++;
+    t->status = RUNNING;
+
+    PRINT_DEBUG(10, "Execution of task %u (step %u)\n", t->task_id, t->step);
+    
+    unsigned int result = t->fct(t, t->step);
+    
+    return result;
+}
+
+void terminate_task(task_t *t)
+{
+    t->status = TERMINATED;
+    
+    PRINT_DEBUG(10, "Task terminated: %u\n", t->task_id);
+
+#ifdef WITH_DEPENDENCIES
+    if(t->parent_task != NULL){
+        task_t *waiting_task = t->parent_task;
+        waiting_task->task_dependency_done++;
+        
+        task_check_runnable(waiting_task);
+    }
+#endif
+
+}
+
+void task_check_runnable(task_t *t)
+{
+#ifdef WITH_DEPENDENCIES
+    if(t->task_dependency_done == t->task_dependency_count){
+        t->status = READY;
+        dispatch_task(t);
+    }
+#endif
+}
---
+++ lab6/src/tasks_implem.h
@@ -0,0 +1,26 @@
+#ifndef __TASKS_IMPLEM_H__
+#define __TASKS_IMPLEM_H__
+#include <stdio.h>
+#include <pthread.h>
+#include "tasks_queue.h"
+
+
+#include "tasks_types.h"
+extern tasks_queue_t *tqueue;
+extern pthread_t *workers ;
+extern pthread_mutex_t mutex;
+extern pthread_cond_t empty;
+extern pthread_cond_t full;
+void create_queues(void);
+void delete_queues(void);
+
+void create_thread_pool(void);
+
+void dispatch_task(task_t *t);
+task_t* get_task_to_execute(void);
+unsigned int exec_task(task_t *t);
+void terminate_task(task_t *t);
+
+void task_check_runnable(task_t *t);
+
+#endif
